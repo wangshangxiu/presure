d9715f100462a7229ba5e707f0515ef656666b32
sammy <xiyouji.xiaoyaoyou.lm13@gmail.com>
2020-09-11 20:28:05 +0800

    feat:大样本数据,写redis

---
+++ uvclient/data/32/ins_redis_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cat ins_redis_data.txt.aa | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat ins_redis_data.txt.ab | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat ins_redis_data.txt.ac | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat ins_redis_data.txt.ad | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat ins_redis_data.txt.ae | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat ins_redis_data.txt.af | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat ins_redis_data.txt.ag | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
---
+++ uvclient/data/32/set_0_redis_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cat set_0_redis_data.txt.aa | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat set_0_redis_data.txt.ab | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat set_0_redis_data.txt.ac | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat set_0_redis_data.txt.ad | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat set_0_redis_data.txt.ae | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
+cat set_0_redis_data.txt.af | redis-cli -h 10.3.0.65 -p 19000 -a rxLt2bdQAyf9E  --pipe
